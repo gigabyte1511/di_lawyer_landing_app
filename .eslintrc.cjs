b81999ab5c0259dca1a3607f920cf5138edae848
gigabyte1511 <gigbyte1511@gmail.com>
2023-04-04 00:39:27 +0600

    Migrated at Vite webpack

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:react/jsx-runtime'
+  ],
+  overrides: [ // Используем `overrides`, чтобы ESLint мог проверять файлы как JS, так и TS.
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+      ],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json']
+      }
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'// Для TypeScript.
+  ],
+  rules: {
+  }
+}
