d40ad97b21ec930b50a9e465c58031589d2eccdc
gigabyte1511 <gigbyte1511@gmail.com>
2023-04-05 21:31:24 +0600

    Added contacts section

--- src/components/Sections/ContactSection/ContactSection.tsx
+++ src/components/Sections/ContactSection/ContactSection.tsx
@@ -1,17 +1,31 @@
 import style from './style.module.css'
-import contact from './img/contact.png'
-import ContactForm from '../../ContactForm/ContactForm'
+import instagram from './img/instagram.png'
+import instgram_logo from './img/instagramLogo.svg'
+import telegram from './img/telegram.png'
+import telegram_logo from './img/telegram-logo.svg'
 
 export default function ContactSection (): JSX.Element {
   return (
-    <div className={style.container}>
-      <h1 className={style.header}>Остались вопросы?</h1>
-      <div className={style.imageVrapper}>
-        <img className={style.image} src={contact} alt="" />
-        <div className={style.form}>
-          <ContactForm />
+    <section className={style.container}>
+      <h1 className={style.header}>Связаться со мной</h1>
+      <div className={style.messengers}>
+        <div className={style.instagram}>
+          <img src={instagram} alt="" />
+        </div>
+        <div className={style.telegramm}>
+          <img src={telegram} alt="" />
         </div>
       </div>
-    </div>
+      <div className={style.buttonContainer}>
+        <button className={style.instButton}>
+          <p>Instagram</p>
+          <img src={instgram_logo} alt="" />
+        </button>
+        <button className={style.telegButton}>
+          <p>Telegram</p>
+          <img src={telegram_logo} alt="" />
+        </button>
+      </div>
+    </section>
   )
 }
