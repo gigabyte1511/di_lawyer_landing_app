8ad943a0f19481cefccaeea08f91580c1348552d
gigabyte1511 <gigbyte1511@gmail.com>
2023-04-06 19:26:49 +0600

    Added grid container

--- src/components/Sections/ContactSection/ContactSection.tsx
+++ src/components/Sections/ContactSection/ContactSection.tsx
@@ -8,23 +8,27 @@ export default function ContactSection (): JSX.Element {
   return (
     <section className={style.container}>
       <h1 className={style.header}>Связаться со мной</h1>
-      <div className={style.messengers}>
-        <div className={style.instagram}>
-          <img src={instagram} alt="" />
-        </div>
-        <div className={style.telegramm}>
-          <img src={telegram} alt="" />
-        </div>
-      </div>
-      <div className={style.buttonContainer}>
-        <button className={style.instButton}>
-          <p>Instagram</p>
-          <img src={instgram_logo} alt="" />
-        </button>
-        <button className={style.telegButton}>
-          <p>Telegram</p>
-          <img src={telegram_logo} alt="" />
-        </button>
+      <div className={style.grid}>
+          <img className={style.imgFirst} src={instagram} alt="" />
+          <div className={style.introFirst}>
+            Чем раньше вы свяжетесь с адвокатом, тем больше шансов выиграть дело и сохранить ваши нервы! Социальные сети и мессенджеры - самый быстрый и удобный на данный момент способ связаться и поддерживать связь со мной.
+          </div>
+          <div className={style.introSecond}>
+            <div>
+              Подписывайтесь на мой Instagram и Telegram, там вы найдете больше информации обо мне, а также узнаете много нового и полезного.
+            </div>
+            <div className={style.buttonContainer}>
+              <button className={style.instButton}>
+                <p>Instagram</p>
+                <img src={instgram_logo} alt="" />
+              </button>
+              <button className={style.telegButton}>
+                <p>Telegram</p>
+                <img src={telegram_logo} alt="" />
+              </button>
+            </div>
+          </div>
+          <img className={style.imgSecond} src={telegram} alt="" />
       </div>
     </section>
   )
