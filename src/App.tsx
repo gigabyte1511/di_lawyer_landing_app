0c5d00f9174d3ff1ed7fa4adb7d9c63695684f79
gigabyte1511 <gigbyte1511@gmail.com>
2023-04-07 19:40:09 +0600

    Added new view points

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 
 function App (): JSX.Element {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
       <Outlet />
     </div>
   )
